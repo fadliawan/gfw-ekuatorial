8d196a2016f6cd5eb0f845908df053adece0af8c
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2015-03-08 00:25:17 +0700

    Init non-full-bleed carousel

--- app/js/story.js
+++ app/js/story.js
@@ -4,6 +4,11 @@
 
   $('.js-image-carousel').jCarouselLite({
     auto: true,
+    timeout: 5*1000
+  });
+
+  $('.js-image-carousel--full-bleed').jCarouselLite({
+    auto: true,
     timeout: 5*1000,
     responsive: true
   });
