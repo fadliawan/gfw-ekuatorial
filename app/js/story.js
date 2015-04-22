bb97c483cef6e895e0c00f8516c11e0b9d152b40
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2015-04-22 07:45:34 +0700

    Add video with map (WIP)

--- app/js/story.js
+++ app/js/story.js
@@ -1,3 +1,4 @@
+// Carousel
 (function($) {
 
   "use strict";
@@ -38,5 +39,48 @@
     $('.js-image-carousel--full-bleed').trigger('refreshCarousel');
   })
   .trigger('resize');
+
+})(jQuery);
+
+// Mapbox GL
+(function($) {
+
+  "use strict";
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZWt1YXRvcmlhbCIsImEiOiJGM0JvYm9nIn0.u9T-Oicf307a3MwSd5AfqQ';
+
+  var VideoWithMap = {
+
+    data: null,
+
+    map: null,
+
+    $video: $('.js-swm-video'),
+
+    init: function() {
+      this.initMap();
+    },
+
+    initText: function() {
+
+
+    },
+
+    initVideo: function() {
+
+    },
+
+    initMap: function() {
+      this.map = new mapboxgl.Map({
+        container: 'swm-map',
+        style: 'https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v7.json',
+        center: [-2.6160421, 114.5014161],
+        zoom: 9
+      });
+    }
+
+  };
+
+  VideoWithMap.init();
 
 })(jQuery);
